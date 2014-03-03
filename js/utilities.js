e6f461680e26ca99237af1ec2c625acf5b2467c8
Kevin McDermott <kevin@bigkevmcd.com>
2014-03-03 19:47:47 +0000

    First commit.

---
+++ js/utilities.js
@@ -0,0 +1,68 @@
+var parseDate = d3.time.format("%m/%d/%y %H:%M").parse;
+
+d3.csv("power.csv", function(error, data) {
+  data.forEach(function(d) {
+     d.timestamp = parseDate(d.timestamp);
+     d.current = +d.current;
+  });
+  var dataSources = [
+    {
+     id: 'power-timeline',
+     title: 'kw/h',
+     dimensions: {
+        x: 'timestamp',
+        y: 'current',
+     },
+     data: data
+   }
+  ];
+  var timelineGraph = glimpse.graphBuilder.create('line')
+                        .data(dataSources)
+                        .config({yAxisUnit: ' kw/h', width: 780, height: 580})
+                        .render('#power');
+});
+
+d3.csv("temperature.csv", function(error, data) {
+  data.forEach(function(d) {
+     d.timestamp = parseDate(d.timestamp);
+     d.temp = +d.temp;
+  });
+
+  var dataSources = [
+    {
+     id: 'temperature-conservatory',
+     title: 'Conservatory Temperature',
+     dimensions: {
+        x: 'timestamp',
+        y: 'temp',
+     },
+     data: data
+   }
+  ];
+  var timelineGraph = glimpse.graphBuilder.create('line')
+                        .data(dataSources)
+                        .config({yAxisUnit: ' C', width: 780, height: 580})
+                        .render('#temperature');
+});
+
+d3.csv("humidity.csv", function(error, data) {
+  data.forEach(function(d) {
+     d.timestamp = parseDate(d.timestamp);
+     d.humidity = +d.humidity;
+  });
+  var dataSources = [
+    {
+     id: 'humidity-conservatory',
+     title: 'Conservatory Humidity',
+     dimensions: {
+        x: 'timestamp',
+        y: 'humidity',
+     },
+     data: data
+   }
+  ];
+  var timelineGraph = glimpse.graphBuilder.create('line')
+                        .data(dataSources)
+                        .config({yAxisUnit: ' %', width: 780, height: 580})
+                        .render('#humidity');
+});
